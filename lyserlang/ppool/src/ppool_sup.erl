2c005e08cb67fa01e81171eac35eb20689104a91
Kristoffer Dalby <kradalby@kradalby.no>
2016-03-27 21:11:47 +0200

    adds ppool lyse exercise;

---
+++ lyserlang/ppool/src/ppool_sup.erl
@@ -0,0 +1,24 @@
+-module(ppool_sup).
+-behaviour(supervisor).
+
+-export([start_link/3]).
+
+-export([init/1]).
+
+
+start_link(Name, Limit, MFA) ->
+    supervisor:start_link(?MODULE, {Name, Limit, MFA}).
+
+init({Name, Limit, MFA}) ->
+    MaxRestart = 1,
+    MaxTime = 3600,
+    {ok, {
+        {one_for_all, MaxRestart, MaxTime},
+        [{
+        serv, {ppool_serv, start_link, [Name, Limit, self(), MFA]},
+            permanent,
+            5000,
+            worker,
+            [ppool_serv]
+        }]
+    }}.
