2b850f2594639213a4e9a60c04804e7a673c852d
Kristoffer Dalby <kradalby@kradalby.no>
2016-03-28 14:06:16 +0200

    adds spec to functions and passes dialyzer

--- ex5/bank/src/bank.erl
+++ ex5/bank/src/bank.erl
@@ -18,30 +18,39 @@
 
 -define(SERVERNAME, bank_serv).
 
+-spec start('normal',_) -> {'ok', pid()}.
 start(normal, _Args) ->
     bank_sup:start_link().
 
+-spec stop(_) -> 'ok'.
 stop(_State) ->
     ok.
 
+-spec new(number()) -> boolean().
 new(AccountNumber) ->
     gen_server:call(?SERVERNAME, {new, AccountNumber}).
 
+-spec withdraw(number(),number()) -> number().
 withdraw(AccountNumber, Amount) ->
     gen_server:call(?SERVERNAME, {withdraw, AccountNumber, Amount}).
 
+-spec deposit(number(),number()) -> number().
 deposit(AccountNumber, Amount) ->
     gen_server:call(?SERVERNAME, {deposit, AccountNumber, Amount}).
 
+-spec transfer(number(),number(),number()) -> number().
 transfer(FromAccount, ToAccount, Amount) ->
     gen_server:call(?SERVERNAME, {transfer, FromAccount, ToAccount, Amount}).
 
+-spec balance(number()) -> number().
 balance(AccountNumber) ->
     gen_server:call(?SERVERNAME, {balance, AccountNumber}).
 
+-spec information() -> list().
 information() ->
     gen_server:call(?SERVERNAME, {info}).
 
 
+-spec fail() -> 'ok'.
 fail() ->
     gen_server:cast(?SERVERNAME, {fail}).
--- ex5/bank/src/bank_serv.erl
+++ ex5/bank/src/bank_serv.erl
@@ -17,12 +17,15 @@
 ]).
 
 
+-spec init([]) -> {'ok',[]}.
 init([]) ->
     {ok, []}.
 
+-spec terminate(_,list()) -> 'ok'.
 terminate(_Reason, _State) ->
     ok.
 
+-spec handle_call(any(),tuple(),list()) -> {'noreply',_} | {'reply',_,_}.
 handle_call({new, AccountNumber}, _From, State) ->
     {NewState, Response} = new(State, AccountNumber),
     {reply, Response, NewState};
@@ -46,39 +49,46 @@ handle_call({balance, AccountNumber}, _From, State) ->
 handle_call({info}, _From, State) ->
     {reply, State, State};
 
-
 handle_call(_Pattern, From, State) ->
     io:format("Received bad call from: ~p~n", [From]),
     {noreply, State}.
 
+-spec handle_cast(any(),list()) -> {'noreply',_}.
 handle_cast({fail}, State) ->
-    2/0;
+    2/0,
+    {noreply, State};
 
 handle_cast(_Pattern, State) ->
     io:format("Received bad cast"),
     {noreply, State}.
 
+-spec handle_info(_,list()) -> {'noreply',_}.
 handle_info(Info, State) ->
     io:format("Unexpected message: ~p~n", [Info]),
     {noreply, State}.
 
+-spec code_change(_,list(),_) -> {'ok',_}.
 code_change(_OldVersion, State, _Extra) ->
     {ok, State}.
 
 
+-spec start_link([]) -> 'ignore' | {'error',_} | {'ok',pid()}.
 start_link([]) ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+-spec stop() -> 'ok' | {'ok',_}.
 stop() ->
     gen_server:stop(?MODULE).
 
 
+-spec new(maybe_improper_list(),number()) -> {maybe_improper_list(),boolean()}.
 new(Accounts, AccountNumber) ->
     case lists:keyfind(AccountNumber, 1, Accounts) of
         false -> {[{AccountNumber, 0}|Accounts], true};
         {AccountNumber, _Amount} -> {Accounts, false}
     end.
 
+-spec withdraw(maybe_improper_list(),number(),number()) -> {maybe_improper_list(),number()}.
 withdraw(Accounts, AccountNumber, Amount) ->
     case lists:keyfind(AccountNumber, 1, Accounts) of
         false -> {Accounts, 0};
@@ -95,6 +105,7 @@ withdraw(Accounts, AccountNumber, Amount) ->
             end
         end.
 
+-spec deposit(maybe_improper_list(),number(),number()) -> {maybe_improper_list(),number()}.
 deposit(Accounts, AccountNumber, Amount) ->
     case lists:keyfind(AccountNumber, 1, Accounts) of
         false -> {Accounts, 0};
@@ -108,6 +119,7 @@ deposit(Accounts, AccountNumber, Amount) ->
                {NewAccounts, CurrentAmount + Amount}
         end.
 
+-spec transfer(maybe_improper_list(),number(),number(),number()) -> {maybe_improper_list(),_}.
 transfer(Accounts, FromAccount, ToAccount, Amount) ->
     case withdraw(Accounts, FromAccount, Amount) of
         {_NewAccounts, 0} -> {Accounts, 0};
@@ -116,6 +128,7 @@ transfer(Accounts, FromAccount, ToAccount, Amount) ->
             {NewAccounts2, Amount}
     end.
 
+-spec balance(maybe_improper_list(),number()) -> number().
 balance(Accounts, AccountNumber) ->
     case lists:keyfind(AccountNumber, 1, Accounts) of
         false -> 0;
